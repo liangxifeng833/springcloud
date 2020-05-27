77f733530deb2c603fbbc7a4134478f03f1e53bc
liangxifeng <liangxifeng833@163.com>
2020-05-27 14:14:23 +0800

    add zk-order server

---
+++ zookeeper/order/src/main/java/lxf/order/OrderApplication.java
@@ -0,0 +1,29 @@
+package lxf.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+//@EnableDiscoveryClient 作用是使用zookeeper或connsul作为注册中心所用
+//向注册中心注册服务
+@EnableDiscoveryClient
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+    //手动将RestTemplate加入的bean中
+    @Bean
+    //如果使用rest方式以别名方式进行调用,则依赖ribbon负载均衡器@LoadBalanced
+    //让RestTemplate请求时拥有客户端负载均衡能力
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
---
+++ zookeeper/order/src/main/java/lxf/order/api/OrderApiController.java
@@ -0,0 +1,22 @@
+package lxf.order.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OrderApiController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/getOrder")
+    public String getOrder(){
+        //两种方式调用member服务接口，一种是直接通过member ipd地址调用，另一种是通过注册中心获取member的ip地址然后调用
+        //String res = restTemplate.getForObject("http://127.0.0.1:8000/getMember",String.class);
+        //使用别名的方式请求member服务接口
+        String res = restTemplate.getForObject("http://zk-member/getMember",String.class);
+        System.out.println("订单服务调用会员服务getMember结果res="+res);
+        return res;
+    }
+}
