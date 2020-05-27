7a20e85062eb7a6512cd37f3976e34cb5a7c7939
liangxifeng <liangxifeng833@163.com>
2020-05-27 14:42:30 +0800

    modify order

--- zookeeper/order/src/main/java/lxf/order/api/OrderApiController.java
+++ zookeeper/order/src/main/java/lxf/order/api/OrderApiController.java
@@ -1,14 +1,22 @@
 package lxf.order.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class OrderApiController {
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/getOrder")
     public String getOrder(){
@@ -18,5 +26,18 @@ public class OrderApiController {
         String res = restTemplate.getForObject("http://zk-member/getMember",String.class);
         System.out.println("订单服务调用会员服务getMember结果res="+res);
         return res;
+    }
+
+    /**
+     * 可以获取服务中心的服务列表等数据
+     * @return
+     */
+    @RequestMapping("/discoveryClientMember")
+    public List<ServiceInstance> discoveryClientMember() {
+        List<ServiceInstance> instance = discoveryClient.getInstances("zk-member");
+        for (ServiceInstance serviceInstance : instance) {
+            System.out.println("zk-member url:"+serviceInstance.getUri());
+        }
+        return instance;
     }
 }
