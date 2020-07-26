e7a142a34fd779a9222648bd673095ada83a2bbd
liangxifeng <liangxifeng833@163.com>
2020-07-26 11:50:31 +0800

    修改分布式配置中心client手动刷新

--- configCenter/config2.0_client/src/main/java/configClient/TestController.java
+++ configCenter/config2.0_client/src/main/java/configClient/TestController.java
@@ -1,10 +1,14 @@
 package configClient;
-
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//手动触发刷新本地读取git中配置文件缓存所用
+//访问http://127.0.0.1:8102/actuator/refresh接口刷新本地缓存
+//注意：请求方式：[post]，请求header:[content-type:application/json]
+@RefreshScope
 public class TestController {
     //读取git中的配置文件的testUrl值
     @Value("${testUrl}")
